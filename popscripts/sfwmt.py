0463a35de2b985067ed3d923f8ab22254d064f14
omehling <o.m.mehling@uu.nl>
2025-08-15 14:17:03 +0200

    Fixed sign error in SFWMT

--- popscripts/sfwmt.py
+++ popscripts/sfwmt.py
@@ -8,13 +8,13 @@ import gsw_xarray as gsw
 rho0 = 1026 # sea water density [kg/m^3]
 cp = 3985 # specific heat capacity [J/kg/K]
 
-def swmt_inputs(pop_mon):
+def swmt_inputs(pop_mon, p=0):
     sss = pop_mon["SALT"].isel(k=0).load()*1000
     sst = pop_mon["TEMP"].isel(k=0).load()
     sst_conservative = gsw.CT_from_pt(SA=sss, pt=sst)
 
-    rho_surf, alpha_surf, beta_surf = gsw.rho_alpha_beta(SA=sss, CT=sst_conservative, p=0)
-    f_heat = alpha_surf/cp*pop_mon["SHF"].load()
+    rho_surf, alpha_surf, beta_surf = gsw.rho_alpha_beta(SA=sss, CT=sst_conservative, p=p)
+    f_heat = -alpha_surf/cp*pop_mon["SHF"].load()
     f_heat.name = "buoyancy_flux_due_to_shf"
     f_heat.attrs = {"long_name": "Surface buoyancy flux due to surface heat flux", "units": "kg/m^2/s"}
     f_fw = beta_surf*1000*(sss/1000/(1-sss/1000))*pop_mon["SFWF"].load()*-1
@@ -25,18 +25,20 @@ def swmt_inputs(pop_mon):
     f_fw_prefac = beta_surf*1000*(sss/1000/(1-sss/1000))
     f_fw_prefac.name = "f_fw_prefac"
 
-    #f_surf = (f_heat+f_fw)
-    #f_surf.name = "buoyancy_flux"
-    #f_surf.attrs = {"long_name": "Surface buoyancy flux", "units": "kg/m^2/s"}
-    try:
+    f_surf = (f_heat+f_fw)
+    f_surf.name = "buoyancy_flux"
+    f_surf.attrs = {"long_name": "Surface buoyancy flux", "units": "kg/m^2/s"}
+
+    if p==0 and "PD" in list(pop_mon.data_vars):
         pd_surf = pop_mon["PD"].isel(k=0).load()*1000
-    except KeyError:
+    else:
         pd_surf = rho_surf
-    pd_surf = pd_surf.where(pd_surf>=1000)
+    #pd_surf = pd_surf.where(pd_surf>=1000)
+    pd_surf = pd_surf.where(pd_surf>pd_surf.min()+1)
     pd_surf.name = "density"
     pd_surf.attrs = {"long_name": "Surface density", "units": "kg/m^3"}
 
-    return xr.merge([f_heat, f_fw, pd_surf, alpha_surf, f_fw_prefac])
+    return xr.merge([f_surf, f_heat, f_fw, pd_surf, alpha_surf, f_fw_prefac])
 
 def swmt(buoyancy_flux, density, tarea, mask, sigmas):
     swmt_calc = []
@@ -63,10 +65,16 @@ def swmt(buoyancy_flux, density, tarea, mask, sigmas):
 
     return swmt_xr
 
-def swmt_from_inputs(swmt_in, mask, sigmas):
-    pass    
+def swmt_from_inputs(swmt_in, grid, mask, sigmas):
+    return swmt(
+        swmt_in["buoyancy_flux"],
+        swmt_in["density"],
+        grid["TAREA"]*1e-4,
+        mask,
+        sigmas
+    )
 
-def swmt_components_from_inputs(swmt_in, mask, sigmas):
+def swmt_components_from_inputs(swmt_in, grid, mask, sigmas):
     pass
 
 def volume_below_sigma(cell_volume, density, mask, sigmas):
