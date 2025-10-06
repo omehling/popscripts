7905aa60cab8a8c959e4ed7c2f16b0d7d73e5e11
reykboerner <r.borner@uu.nl>
2025-10-06 11:48:00 +0200

    made transport function compute all latitudes

--- src/pop_diagnostics/__init__.py
+++ src/pop_diagnostics/__init__.py
@@ -53,34 +53,35 @@ class POP():
         """
         Returns net surface freshwater flux in region mask, decomposed into components.
         """
-        P = data.PRECIP
-        E = data.EVAP
-        R = data.RUNOFF # kg/m^2/s
-        rest = data.S_WEAK_REST
-        melt = data.SFWF - (P + E + R + rest)
-        return [data.SFWF, P, E, R, melt]
+        P = data.PRECIP.where(mask)
+        E = data.EVAP.where(mask)
+        R = data.RUNOFF.where(mask) # kg/m^2/s
+        rest = data.S_WEAK_REST.where(mask)
+        total = data.SFWF.where(mask) 
+        melt = total - (P + E + R + rest)
+        return [total, P, E, R, melt]
     
-    def F_transport(self, data, mask=None, latidx=85, S0=35):
+    def F_transport(self, data, mask=None, S0=35):
         if mask is None:
             mask = self.mask2D('Atlantic')
             
-        S = data.SALT.where(self.mask3D(mask)).isel(j=latidx)*1000 # g/kg
-        v = data.VVEL.where(self.mask3D(mask)).isel(j=latidx)/100 # m/s
+        S = data.SALT.where(self.mask3D(mask))*1000 # g/kg
+        v = data.VVEL.where(self.mask3D(mask))/100 # m/s
 
-        v_zonav = v.weighted(self.grid.DXU.isel(j=latidx)).mean(dim='i')
+        v_zonav = v.weighted(self.grid.DXU).mean(dim='i')
         v_secav = v_zonav.weighted(self.grid.dz).mean(dim='k')
         v_star = v - v_secav
         v_prime = v - v_zonav
 
-        S_zonav = S.weighted(self.grid.DXT.isel(j=latidx)).mean(dim='i')
+        S_zonav = S.weighted(self.grid.DXT).mean(dim='i')
         S_secav = S_zonav.weighted(self.grid.dz).mean(dim='k')
         S_prime = S - S_zonav
 
-        dx = self.grid.DXU.isel(j=latidx)/100
+        dx = self.grid.DXU/100
         dz = self.grid.dz
 
         # barotropic
-        F_bt = - v_secav*(S_secav-S0)/S0*((dx*dz).where(self.mask3D(mask).isel(j=latidx)).sum())*1e-6 # Sv
+        F_bt = - v_secav*(S_secav-S0)/S0*((dx*dz).where(self.mask3D(mask)).sum(dim=['i', 'k']))*1e-6 # Sv
         # overturning
         F_ov = - ((v_star*dx).sum(dim='i')*(S_zonav - S0)*dz).sum(dim='k')/S0*1e-6 # Sv
         # azimuthal
